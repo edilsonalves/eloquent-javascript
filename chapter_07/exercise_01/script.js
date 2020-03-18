72fbcc9fe340165f37de1fc89abeae50fb7bb991
Edilson Alves <email@edilsonalves.com>
2020-03-18 19:15:46 -0300

    Remove support code (Chapter 07)

--- chapter_07/exercise_01/script.js
+++ chapter_07/exercise_01/script.js
@@ -1,15 +1,3 @@
-function countSteps(state, robot, memory) {
-  for (let steps = 0; ; steps++) {
-    if (state.parcels.length === 0) {
-      return steps;
-    }
-
-    let action = robot(state, memory);
-    state = state.move(action.direction);
-    memory = action.memory;
-  }
-}
-
 function compareRobots(robot1, memory1, robot2, memory2) {
   let total1 = 0;
   let total2 = 0;
